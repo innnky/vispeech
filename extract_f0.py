27532cd125b88506f5880c431fb90ec7950f8d67
innnky <xiyiji508@gmail.com>
2022-11-19 12:59:55 +0800

    add audio sample

--- extract_f0.py
+++ extract_f0.py
@@ -1,36 +1,36 @@
-filelist = "filelists/train.txt"
+filelist = "filelists/paimon_extend.txt.preprocessd"
 import librosa
 import pyworld
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
-# import parselmouth
+import parselmouth
 fs = 22050
 hop = 256
 #
-# def get_pitch(path,lll):
-#     """
-#     :param wav_data: [T]
-#     :param mel: [T, 80]
-#     :param config:
-#     :return:
-#     """
-#     sampling_rate = fs
-#     hop_length = hop
-#     wav_data, _ = librosa.load(path,sampling_rate)
-#     time_step = hop_length / sampling_rate * 1000
-#     f0_min = 80
-#     f0_max = 750
-#
-#     f0 = parselmouth.Sound(wav_data, sampling_rate).to_pitch_ac(
-#         time_step=time_step / 1000, voicing_threshold=0.6,
-#         pitch_floor=f0_min, pitch_ceiling=f0_max).selected_array["frequency"]
-#     lpad = 2
-#     rpad = lll - len(f0) - lpad
-#     assert 0<=rpad<=2
-#     f0 = np.pad(f0, [[lpad, rpad]], mode="constant")
-#
-#     return f0
+def get_pitch(path,lll):
+    """
+    :param wav_data: [T]
+    :param mel: [T, 80]
+    :param config:
+    :return:
+    """
+    sampling_rate = fs
+    hop_length = hop
+    wav_data, _ = librosa.load(path,sampling_rate)
+    time_step = hop_length / sampling_rate * 1000
+    f0_min = 80
+    f0_max = 750
+
+    f0 = parselmouth.Sound(wav_data, sampling_rate).to_pitch_ac(
+        time_step=time_step / 1000, voicing_threshold=0.6,
+        pitch_floor=f0_min, pitch_ceiling=f0_max).selected_array["frequency"]
+    lpad = 2
+    rpad = lll - len(f0) - lpad
+    assert 0<=rpad<=2
+    f0 = np.pad(f0, [[lpad, rpad]], mode="constant")
+
+    return f0
 #
 
 def compute_f0(path):
@@ -56,11 +56,13 @@ with open(filelist) as f:
         wavpath, raw_phones, raw_durations, spk = line.split("|")[0], line.split("|")[1], line.split("|")[2], line.split("|")[3]
         durations = [int(i) for i in raw_durations.split(" ")]
         phones = raw_phones.split(" ")
-        f0 = compute_f0(wavpath)
+        f0 = get_pitch(wavpath, sum(durations))
         phf0s = []
         current_frame = 0
         for dur in durations:
-            phf0s.append(np.average(f0[current_frame:current_frame+dur]))
+            chunk = f0[current_frame:current_frame+dur]
+            phf0s.append(np.average(chunk[chunk!=0]))
+            # phf0s.append(np.average(chunk))
             current_frame+=dur
         assert abs(current_frame - len(f0)) <2
         phf0s = " ".join(['{:.3f}'.format(i) for i in phf0s])
@@ -69,7 +71,7 @@ with open(filelist) as f:
         print(wavpath, i, phf0s)
         i += 1
         np.save(wavpath+".f0.npy", f0)
-with open(filelist, "w") as f:
+with open(filelist+"newnew", "w") as f:
     f.write(outstr)
 
 
