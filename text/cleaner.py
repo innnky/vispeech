7f42ad19af5cbbc065c989cf90fa031ae9ba360b
innnky <xiyiji508@gmail.com>
2023-02-07 09:53:03 +0800

    add pinyin cleaner

--- text/cleaner.py
+++ text/cleaner.py
@@ -4,6 +4,7 @@ from text import cleaned_text_to_sequence
 from text.en_frontend import en_to_phonemes
 from text.ja_frontend import ja_to_phonemes
 from text.zh_frontend import zh_to_phonemes
+from text.zh_frontend import pinyin_to_phonemes
 from text.mix_frontend import others_to_phonemes
 
 def str_replace( data):
@@ -21,9 +22,9 @@ def text_to_sequence(text):
 
 def text_to_phones(text: str) -> list:
     text = str_replace(text).replace("\"", '')
-    # find all text blocks enclosed in [JA], [ZH], [EN]
+    # find all text blocks enclosed in [JA], [ZH], [EN], [P]
     original_text = text
-    blocks = re.finditer(r'\[(JA|ZH|EN)\](.*?)\[\1\]', text)
+    blocks = re.finditer(r'\[(JA|ZH|EN|P)\](.*?)\[\1\]', text)
     phonemes = []
     last_end = 0
     for block in blocks:
@@ -34,6 +35,8 @@ def text_to_phones(text: str) -> list:
         last_end = end
         language = block.group(1)
         text = block.group(2)
+        if language == 'P':
+            phonemes += pinyin_to_phonemes(text)
         if language == 'JA':
             phonemes += ja_to_phonemes(text)
         elif language == 'ZH':
@@ -48,15 +51,13 @@ def text_to_phones(text: str) -> list:
 
 if __name__ == '__main__':
     test_text = "[JA]こんにちは。こんにちは\}{ll[JA]abc你好[ZH]你好[ZH][EN]Hello你好.vits![EN][JA]こんにちは。[JA]"
-    # # 我需要编写一个文本前端，已经编写好zh_to_phonemes(text:str)->list en_to_phonemes(text:str)->list ja_to_phonemes(text:str)->list others_to_phonemes(text:str)->list
-    # # 对于test_text 模式的输入文本，需要找到每一个[JA]包裹的块执行ja_to_phonemes，zh和en同理。对于没有被任何块包裹的,则执行others_to_phonemes 最终需要将这些方法返回的音素序列按照原本的顺序合成一个完整的音素序列
-    # # 请直接给出完整代码
     text = "借还款,他只是一个纸老虎，开户行，奥大家好33啊我是Ab3s,?萨达撒abst 123、~~、、 但是、、、A B C D!"
     text = "奥大家,好33啊,こんにちは我是Ab3s,?萨达撒abst 123、~~、、 但*是、、、A B C D!"
     text = '[JA]シシ…すご,,いじゃんシシラシャミョンありがとうえーっとシンモーレシンモーレじゃんシシラシャミョン[JA]'
     text="大家好#要筛#，你好#也#要筛#。"
     text = "嗯？什么东西…沉甸甸的…下午1:00，今天是2022/5/10"
     text = "A I人工智能"
+    text = '[P]pin1 yin1 zhen1 hao3 wan2[P]'
     # text = "[JA]それより仮屋さん,例の件ですが――[JA]"
     # text = "早上好，今天是2020/10/29，最低温度是-3°C。"
     # # text = "…………"
--- text/zh_frontend.py
+++ text/zh_frontend.py
@@ -7,7 +7,12 @@ frontend = Frontend()
 pu_symbols = ['!', '?', '…', ",", "."]
 
 # print(_symbol_to_id)
+pinyin2phones = {}
 
+for line in open("text/zh_dict.dict").readlines():
+    pinyin, phones = line.strip().split("\t")
+    phones = phones.split(" ")
+    pinyin2phones[pinyin] = phones
 
 def pu_symbol_replace(data):
     chinaTab = ['！', '？', "…", "，", "。",'、', "..."]
@@ -31,3 +36,13 @@ def zh_to_phonemes(text):
     text = pu_symbol_replace(text)
     phones = frontend.get_phonemes(text)[0]
     return remove_invalid_phonemes(phones)
+
+
+def pinyin_to_phonemes(text):
+    phones = []
+    for pinyin in text.split(" "):
+        try:
+            phones += pinyin2phones[pinyin]
+        except:
+            print("词典中无此拼音：", pinyin)
+    return phones
