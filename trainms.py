066342c468b2b035266a9d04db1a89a63e670c0a
innnky <xiyiji508@gmail.com>
2023-02-09 16:50:53 +0800

    remove fftblock

--- trainms.py
+++ trainms.py
@@ -294,7 +294,7 @@ def evaluate(hps, generator, eval_loader, writer_eval):
                 wav_lengths = wav_lengths[:1]
                 # break
                 y_hat, mask, xx,  duration, pred_f0, energy = generator.module.infer(phonemes, phonemes_lengths,spec,
-                                                         max_len=1000, sid=sid,pitch_control=shift,energy_control=energy_shift)
+                                                         max_len=1000)
                 y_hat_lengths = mask.sum([1, 2]).long() * hps.data.hop_length
 
                 mel = spec_to_mel_torch(
